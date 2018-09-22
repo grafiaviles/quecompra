1de0a94a08b6f834816e6721d2a688d65e62be75
grafiaviles <grafiaviles@gmail.com>
2018-09-22 01:59:46 -0300

    update section

--- gulpfile.js
+++ gulpfile.js
@@ -11,88 +11,88 @@ const fs = require('fs');
 
 // CSS Tasks
 gulp.task('css-compile', function() {
-  gulp.src('scss/**/*.scss')
-    .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 10 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('./dist/css/'));
+    gulp.src('scss/**/*.scss')
+        .pipe(sass({ outputStyle: 'nested' }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('css-minify', function() {
     gulp.src(['./dist/css/*.css', '!dist/css/*.min.css'])
-      .pipe(cssmin())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('./dist/css'));
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 // JavaScript Tasks
 gulp.task('js-build', function() {
 
-  const plugins = getJSModules();
+    const plugins = getJSModules();
 
-  gulp.src(plugins.modules)
-    .pipe(concat('mdb.js'))
-    .pipe(gulp.dest('./dist/js/'));
+    gulp.src(plugins.modules)
+        .pipe(concat('mdb.js'))
+        .pipe(gulp.dest('./dist/js/'));
 
     return gulp.start('js-minify');
 
 });
 
 gulp.task('js-minify', function() {
-  gulp.src('./dist/js/mdb.js')
-    .pipe(minify({
-      ext:{
-        // src:'.js',
-        min:'.min.js'
-      },
-      noSource: true,
-    }))
-    .pipe(gulp.dest('./dist/js'));
+    gulp.src('./dist/js/mdb.js')
+        .pipe(minify({
+            ext: {
+                // src:'.js',
+                min: '.min.js'
+            },
+            noSource: true,
+        }))
+        .pipe(gulp.dest('./dist/js'));
 });
 
 // Image Compression
 gulp.task('img-compression', function() {
-  gulp.src('./img/*')
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
-        ]
-      })
-    ]))
-    .pipe(gulp.dest('./dist/img'));
+    gulp.src('./img/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./dist/img'));
 });
 
 // Live Server
 gulp.task('live-server', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./dist",
-      directory: true
-    },
-    notify: false
-  });
+    browserSync.init({
+        server: {
+            baseDir: "./dist",
+            directory: true
+        },
+        notify: false
+    });
 
-  gulp.watch("**/*", {cwd: './dist/'}, browserSync.reload);
+    gulp.watch("**/*", { cwd: './dist/' }, browserSync.reload);
 });
 
 // Watch on everything
-gulp.task('mdb-go', function() {
-  gulp.start('live-server');
-  gulp.watch("scss/**/*.scss", ['css-compile']);
-  gulp.watch(["dist/css/*.css", "!dist/css/*.min.css"], ['css-minify']);
-  gulp.watch("js/**/*.js", ['js-build']);
-  gulp.watch(["dist/js/*.js", "!dist/js/*.min.js"], ['js-minify']);
-  gulp.watch("**/*", {cwd: './img/'}, ['img-compression']);
+gulp.task('default', function() {
+    gulp.start('live-server');
+    gulp.watch("scss/**/*.scss", ['css-compile']);
+    gulp.watch(["dist/css/*.css", "!dist/css/*.min.css"], ['css-minify']);
+    gulp.watch("js/**/*.js", ['js-build']);
+    gulp.watch(["dist/js/*.js", "!dist/js/*.min.js"], ['js-minify']);
+    gulp.watch("**/*", { cwd: './img/' }, ['img-compression']);
 });
 
 function getJSModules() {
-  delete require.cache[require.resolve('./js/modules.js')];
-  return require('./js/modules');
+    delete require.cache[require.resolve('./js/modules.js')];
+    return require('./js/modules');
 }
